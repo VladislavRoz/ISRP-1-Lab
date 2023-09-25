4c1971eea0c30c0733bca2a4478d1674db50405e
VladislavRoz <rozanov2002@mail.ru>
2023-09-25 17:33:11 +0600

    Программа

--- ISRP 1/Source.cpp
+++ ISRP 1/Source.cpp
@@ -5,7 +5,7 @@ void main()
 {
 	setlocale(LC_ALL, "Rus");
 	int Var1, Var2;
-	int result;
+	int Var3;
 
 	cout << "Введите два числа " << endl;
 	cout << "Первое число: ";
@@ -23,18 +23,18 @@ void main()
 	switch (a)
 	{
 	    case 1:
-		   result = Var1 + Var2;
-		   cout << " Результат:" << result << endl;
+		   Var3 = Var1 + Var2;
+		   cout << " Результат:" << Var3 << endl;
 		   break;
 
 		case 2:
-			result = Var1 - Var2;
-			cout << " Результат:" << result << endl;
+			Var3 = Var1 - Var2;
+			cout << " Результат:" << Var3 << endl;
 			break;
 
 		case 3:
-			result = Var1 * Var2;
-			cout << " Результат:" << result << endl;
+			Var3 = Var1 * Var2;
+			cout << " Результат:" << Var3 << endl;
 			break;
 
 		case 4:
@@ -45,8 +45,8 @@ void main()
 			}
 			else
 			{
-				result = Var1 / Var2;
-				cout << " Результат:" << result << endl;
+				Var3 = Var1 / Var2;
+				cout << " Результат:" << Var3 << endl;
 				break;
 			}
 
@@ -55,5 +55,50 @@ void main()
 			break;
 	}
 
+	for (;;)
+	{
+		int Var4;
+		cout << " Введите число:";
+		cin >> Var4;
+
+		cout << " Выберите операцию : Сложение(1), Вычитание(2), Умножение(3), Деление(4)." << endl;
+
+		int b;
+		cin >> b;
+		switch (b)
+		{
+		  case 1:
+			  Var4 = Var3 + Var4;
+			  cout << " Результат:" << Var4 << endl;
+			  break;
+
+		  case 2:
+			  Var4 = Var3 - Var4;
+			  cout << " Результат:" << Var4 << endl;
+			  break;
+
+		  case 3:
+			  Var4 = Var3 * Var4;
+			  cout << " Результат:" << Var4 << endl;
+			  break;
+
+		  case 4:
+			if (Var2 == 0)
+			{
+				cout << " Делить на ноль нельзя." << endl;
+				break;
+			}
+			else
+			{
+				Var4 = Var3 / Var4;
+				cout << " Результат:" << Var4 << endl;
+				break;
+			}
+
+		  default:
+			  cout << " Некорректный ввод." << endl;
+			  break;
+		}
+	}
 }
 
